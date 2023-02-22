f285f98c66cd027d6e11049322fbdaf99646f8ed
Russell Wallace <russell.wallace@gmail.com>
2023-02-22 23:05:12 +0000

    options

--- src/main/java/sortj/Main.java
+++ src/main/java/sortj/Main.java
@@ -1,13 +1,11 @@
 package sortj;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+
+public final class Main {
+  private static final Option[] OPTIONS = new Option[] {};
+
+  public static void main(String[] args) throws IOException {
+    Option.parse(OPTIONS, args);
+  }
 }
