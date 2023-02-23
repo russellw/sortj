ce590a94e1792cf661a0e1cdd5547a2937e53438
Russell Wallace <russell.wallace@gmail.com>
2023-02-23 00:26:07 +0000

    sort

--- src/main/java/sortj/Main.java
+++ src/main/java/sortj/Main.java
@@ -1,20 +1,28 @@
 package sortj;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public final class Main {
   private static boolean inPlace;
   private static final Option[] OPTIONS =
       new Option[] {
-        new Option("sort files in place", null, "i") {
+        new Option("sort files in place", null, "i", "r", "replace") {
           void accept(String arg) {
             inPlace = true;
           }
         },
       };
 
-  private static void sort(String file) {
-    System.out.println(file);
+  private static void sort(String file) throws IOException {
+    var path = Path.of(file);
+    var in = Files.readAllLines(path, StandardCharsets.UTF_8);
+    var out = new Sort(in).out;
+    if (inPlace) {
+      Files.move(path, Path.of(System.getProperty("java.io.tmpdir")));
+    }
   }
 
   public static void main(String[] args) throws IOException {
---
+++ src/main/java/sortj/Sort.java
@@ -0,0 +1,11 @@
+package sortj;
+
+import java.util.List;
+
+public final class Sort {
+  List<String> out;
+
+  Sort(List<String> in) {
+    out = in;
+  }
+}
