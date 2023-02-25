13aeabbabafa35597bc5d2aefea2fb4021c647ab
Russell Wallace <russell.wallace@gmail.com>
2023-02-25 16:30:44 +0000

    sort

--- src/main/java/sortj/Sort.java
+++ src/main/java/sortj/Sort.java
@@ -6,6 +6,9 @@ public final class Sort {
   List<String> text;
 
   Sort(List<String> text, int i) {
+    assert text.get(i).strip().startsWith("// SORT");
     this.text = text;
+
+    var dent = Etc.indent(text, i);
   }
 }
