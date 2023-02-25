5911d37c1ac1676a805f49460cdcb80f745e3190
Russell Wallace <russell.wallace@gmail.com>
2023-02-25 21:58:13 +0000

    test

--- src/test/java/sortj/ElementTest.java
+++ src/test/java/sortj/ElementTest.java
@@ -3,6 +3,7 @@ package sortj;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import org.junit.Test;
 
 public class ElementTest {
@@ -79,5 +80,29 @@ public class ElementTest {
     e = new Element(text, 1, 0);
     assertEquals(2, e.j);
     assertArrayEquals(new String[] {" a", "  b"}, e.subtext);
+
+    var in =
+        new String[] {
+          "a",
+        };
+    var out =
+        new String[] {
+          "a",
+        };
+    e = new Element(Arrays.asList(in), 0, 0);
+    assertArrayEquals(out, e.subtext);
+    assertEquals(out.length, e.j);
+
+    in =
+        new String[] {
+          "@a", "a", "b",
+        };
+    out =
+        new String[] {
+          "@a", "a",
+        };
+    e = new Element(Arrays.asList(in), 0, 0);
+    assertArrayEquals(out, e.subtext);
+    assertEquals(out.length, e.j);
   }
 }
