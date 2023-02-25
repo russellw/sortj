4fbafa36a9d8cab5792ef3cc35433900b7b77de8
Russell Wallace <russell.wallace@gmail.com>
2023-02-25 14:39:35 +0000

    refactor

--- src/main/java/sortj/Element.java
+++ src/main/java/sortj/Element.java
@@ -3,19 +3,25 @@ package sortj;
 import java.util.List;
 
 public final class Element {
-  final int end;
-  final String[] subtext;
-  final String key;
+  final List<String> text;
+  int end;
+  String[] subtext;
+  String key;
+
+  private int skipBlanks(int i) {
+    while (i < text.size() && text.get(i).isBlank()) i++;
+    return i;
+  }
 
   public Element(List<String> text, int dent, int i) {
+    this.text = text;
+
     // skip leading blank lines
-    while (i < text.size() && text.get(i).isBlank()) i++;
+    i = skipBlanks(i);
 
     // end of file
     if (i == text.size()) {
       end = i;
-      subtext = null;
-      key = null;
       return;
     }
 
@@ -23,23 +29,17 @@ public final class Element {
     if (text.get(i).strip().equals("// END")) {
       if (Etc.indent(i, text.get(i)) != dent) throw new IndentException(i);
       end = i;
-      subtext = null;
-      key = null;
       return;
     }
     if (Etc.indent(i, text.get(i)) < dent) {
       if (!text.get(i).strip().startsWith("}")) throw new IndentException(i);
       end = i;
-      subtext = null;
-      key = null;
       return;
     }
 
     // there is still at least one element to read in this section
-    var j = i;
+    var j = skipBlanks(i);
 
     end = j;
-    subtext = null;
-    key = null;
   }
 }
