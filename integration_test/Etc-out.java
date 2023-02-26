70553aa955a825acd61df325c4a5d89f6287127e
Russell Wallace <russell.wallace@gmail.com>
2023-02-26 18:13:31 +0000

    test

--- integration_test/Etc-out.java
+++ integration_test/Etc-out.java
@@ -9,20 +9,11 @@ public final class Etc {
   static void dbg(Object a) {
     System.out.printf("%s: %s\n", Thread.currentThread().getStackTrace()[2], a);
   }
-
-  static <T> List<T> replace(List<T> in, int i, int j, List<T> out) {
-    var v = new ArrayList<T>();
-    for (var k = 0; k < i; k++) v.add(in.get(k));
-    v.addAll(out);
-    for (var k = j; k < in.size(); k++) v.add(in.get(k));
-    return v;
+  static String ext(String file) {
+    var i = file.lastIndexOf('.');
+    if (i < 0) return "";
+    return file.substring(i + 1);
   }
-
-  static boolean reallyStartsWith(List<String> text, int i, String prefix) {
-    if (i == text.size()) return false;
-    return text.get(i).stripLeading().startsWith(prefix);
-  }
-
   static int indent(List<String> text, int i) {
     // End of file is always the end of a block
     if (i == text.size()) return -1;
@@ -38,13 +29,17 @@ public final class Etc {
     if (s.charAt(j) == '\t') throw new TabException(i);
     return j;
   }
-
-  static String ext(String file) {
-    var i = file.lastIndexOf('.');
-    if (i < 0) return "";
-    return file.substring(i + 1);
+  static boolean reallyStartsWith(List<String> text, int i, String prefix) {
+    if (i == text.size()) return false;
+    return text.get(i).stripLeading().startsWith(prefix);
   }
-
+  static <T> List<T> replace(List<T> in, int i, int j, List<T> out) {
+    var v = new ArrayList<T>();
+    for (var k = 0; k < i; k++) v.add(in.get(k));
+    v.addAll(out);
+    for (var k = j; k < in.size(); k++) v.add(in.get(k));
+    return v;
+  }
   static int skipBlanks(List<String> text, int i) {
     while (i < text.size() && text.get(i).isBlank()) i++;
     return i;
