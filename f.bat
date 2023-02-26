e888f817f6374e66551cb0fbf870b2b9165b5637
Russell Wallace <russell.wallace@gmail.com>
2023-02-26 19:09:26 +0000

    sortj

--- f.bat
+++ f.bat
@@ -1,4 +1,19 @@
+rem Python
 black .
+if %errorlevel% neq 0 goto :eof
+
+rem main
 call google-java-format -i src/main/java/sortj/*.java
+if %errorlevel% neq 0 goto :eof
+
+call sortj              -i src/main/java/sortj/*.java
+if %errorlevel% neq 0 goto :eof
+
+call google-java-format -i src/main/java/sortj/*.java
+if %errorlevel% neq 0 goto :eof
+
+rem test
 call google-java-format -i src/test/java/sortj/*.java
+if %errorlevel% neq 0 goto :eof
+
 git diff
---
+++ sortj.bat
@@ -0,0 +1,1 @@
+java -jar C:\sortj\target\sortj-1.0-SNAPSHOT-jar-with-dependencies.jar %*
