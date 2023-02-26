8f02f6a0d9a37fbd55207840c0e79be8eab9ad90
Russell Wallace <russell.wallace@gmail.com>
2023-02-26 18:56:34 +0000

    refactor

--- f.bat
+++ f.bat
@@ -1,4 +1,4 @@
 black .
-java -jar C:\bin\google-java-format-1.15.0-all-deps.jar -i src/main/java/sortj/*.java
-java -jar C:\bin\google-java-format-1.15.0-all-deps.jar -i src/test/java/sortj/*.java
+call google-java-format -i src/main/java/sortj/*.java
+call google-java-format -i src/test/java/sortj/*.java
 git diff
---
+++ google-java-format.bat
@@ -0,0 +1,1 @@
+java -jar C:\bin\google-java-format-1.15.0-all-deps.jar %*
